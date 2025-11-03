ef3e139d2d6bf1598263852ec5bd01370d89f0d6
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-11-03 11:16:41 +0700

    create payment dto

---
+++ domain/dto/payment.go
@@ -0,0 +1,29 @@
+package dto
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type PaymentRequest struct {
+	OrderID        uuid.UUID      `json:"orderID"`
+	ExpiredAt      time.Time      `json:"expiredAt"`
+	Amount         float64        `json:"amount"`
+	Description    string         `json:"description"`
+	CustomerDetail CustomerDetail `json:"customerDetail"`
+	ItemDetails    []ItemDetails  `json:"itemDetails"`
+}
+
+type CustomerDetail struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+}
+
+type ItemDetails struct {
+	ID       uuid.UUID `json:"id"`
+	Name     string    `json:"name"`
+	Amount   float64   `json:"amount"`
+	Quantity int       `json:"quantity"`
+}
