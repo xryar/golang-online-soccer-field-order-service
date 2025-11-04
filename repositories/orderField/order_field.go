682e3e645372cecca31a70555f0ae890110bbadc
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-11-04 10:50:47 +0700

    create order field repository

---
+++ repositories/orderField/order_field.go
@@ -0,0 +1,43 @@
+package repositories
+
+import (
+	"context"
+	errWrap "order-service/common/error"
+	errConstant "order-service/constants/error"
+	"order-service/domain/models"
+
+	"gorm.io/gorm"
+)
+
+type OrderFieldRepository struct {
+	db *gorm.DB
+}
+
+type IOrderFieldRepository interface {
+	FindByOrderID(context.Context, uint) ([]models.OrderField, error)
+	Create(context.Context, *gorm.DB, []models.OrderField) error
+}
+
+func NewOrderFieldRepository(db *gorm.DB) IOrderFieldRepository {
+	return &OrderFieldRepository{db: db}
+}
+
+func (or *OrderFieldRepository) FindByOrderID(ctx context.Context, orderID uint) ([]models.OrderField, error) {
+	var orderFields []models.OrderField
+
+	err := or.db.WithContext(ctx).Where("order_id = ?", orderID).Find(&orderFields).Error
+	if err != nil {
+		return nil, errWrap.WrapError(errConstant.ErrSQLError)
+	}
+
+	return orderFields, nil
+}
+
+func (or *OrderFieldRepository) Create(ctx context.Context, tx *gorm.DB, request []models.OrderField) error {
+	err := tx.WithContext(ctx).Create(&request).Error
+	if err != nil {
+		return errWrap.WrapError(errConstant.ErrSQLError)
+	}
+
+	return nil
+}
