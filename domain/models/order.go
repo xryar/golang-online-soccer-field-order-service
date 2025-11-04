bbc8e2a705808a3c5032f628d8fbdeef50c29cc6
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-11-04 10:26:43 +0700

    create order model

---
+++ domain/models/order.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"order-service/constants"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Order struct {
+	ID        uint                        `gorm:"primaryKey;autoIncrement"`
+	UUID      uuid.UUID                   `gorm:"type:uuid;not null"`
+	Code      string                      `gorm:"type:varchar(30);not null"`
+	UserId    uuid.UUID                   `gorm:"type:uuid;not null"`
+	PaymentID uuid.UUID                   `gorm:"type:uuid;not null"`
+	Amount    float64                     `gorm:"type:decimal(10,2);not null"`
+	Status    constants.OrderStatusString `gorm:"type:int;not null"`
+	Date      time.Time                   `gorm:"type:timestamp;not null"`
+	IsPaid    bool                        `gorm:"type:boolean;not null"`
+	PaidAt    *time.Time                  `gorm:"type:timestamp"`
+	CreatedAt *time.Time
+	UpdatedAt *time.Time
+}
