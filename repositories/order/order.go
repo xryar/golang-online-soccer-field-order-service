2e5a36874de84a262af876876ecd28194446abaf
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-11-04 11:12:05 +0700

    implement find all with pagination and create new function for find by uuid

--- repositories/order/order.go
+++ repositories/order/order.go
@@ -2,6 +2,9 @@ package repositories
 
 import (
 	"context"
+	"fmt"
+	errWrap "order-service/common/error"
+	errConstant "order-service/constants/error"
 	"order-service/domain/dto"
 	"order-service/domain/models"
 
@@ -16,6 +19,7 @@ type OrderRepository struct {
 type IOrderRepository interface {
 	FindAllWithPagination(context.Context, *dto.OrderRequestParam) ([]models.Order, int64, error)
 	FindByUserID(context.Context, string) ([]models.Order, error)
+	FindByUUID(context.Context, string) (*models.Order, error)
 	Create(context.Context, *gorm.DB, *models.Order) (*models.Order, error)
 	Update(context.Context, *gorm.DB, string, *models.Order, uuid.UUID) error
 }
@@ -24,10 +28,38 @@ func NewOrderRepository(db *gorm.DB) IOrderRepository {
 	return &OrderRepository{db: db}
 }
 
-func (or *OrderRepository) FindAllWithPagination(context.Context, *dto.OrderRequestParam) ([]models.Order, int64, error) {
+func (or *OrderRepository) FindAllWithPagination(ctx context.Context, param *dto.OrderRequestParam) ([]models.Order, int64, error) {
+	var (
+		orders []models.Order
+		sort   string
+		total  int64
+	)
+	if param.SortColumn != nil {
+		sort = fmt.Sprintf("%s %s", *param.SortColumn, *param.SortOrder)
+	} else {
+		sort = "created_at desc"
+	}
+
+	limit := param.Limit
+	offset := (param.Page - 1) * limit
+	err := or.db.WithContext(ctx).Limit(limit).Offset(offset).Order(sort).Find(&orders).Error
+	if err != nil {
+		return nil, 0, errWrap.WrapError(errConstant.ErrSQLError)
+	}
+
+	err = or.db.WithContext(ctx).Model(&models.Order{}).Count(&total).Error
+	if err != nil {
+		return nil, 0, errWrap.WrapError(errConstant.ErrSQLError)
+	}
+
+	return orders, total, nil
 }
 
 func (or *OrderRepository) FindByUserID(context.Context, string) ([]models.Order, error) {
+}
+
+func (or *OrderRepository) FindByUUID(context.Context, string) (*models.Order, error) {
+
 }
 
 func (or *OrderRepository) Create(context.Context, *gorm.DB, *models.Order) (*models.Order, error) {
