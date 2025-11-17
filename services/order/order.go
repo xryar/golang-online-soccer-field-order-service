ed9335dc2faf01ece54a417787822ef62b0f9904
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-11-17 09:49:50 +0700

    setup order service method

--- services/order/order.go
+++ services/order/order.go
@@ -1,1 +1,49 @@
 package services
+
+import (
+	"context"
+	"order-service/clients"
+	"order-service/common/util"
+	"order-service/domain/dto"
+	"order-service/repositories"
+)
+
+type OrderService struct {
+	repositories repositories.IRegistryRepository
+	client       clients.IRegistryClient
+}
+
+type IOrderService interface {
+	GetAllWithPagination(context.Context, *dto.OrderRequestParam) (*util.PaginationResult, error)
+	GetByOrderID(context.Context, string) (*dto.OrderResponse, error)
+	GetOrderByUserID(context.Context) ([]dto.OrderByUserIDResponse, error)
+	Create(context.Context, *dto.OrderRequest) (*dto.OrderResponse, error)
+	HandlePayment(context.Context, *dto.PaymentData) error
+}
+
+func NewOrderService(repositories repositories.IRegistryRepository, client clients.IRegistryClient) IOrderService {
+	return &OrderService{
+		repositories: repositories,
+		client:       client,
+	}
+}
+
+func (os *OrderService) GetAllWithPagination(context.Context, *dto.OrderRequestParam) (*util.PaginationResult, error) {
+	panic("implement me")
+}
+
+func (os *OrderService) GetByOrderID(context.Context, string) (*dto.OrderResponse, error) {
+	panic("implement me")
+}
+
+func (os *OrderService) GetOrderByUserID(context.Context) ([]dto.OrderByUserIDResponse, error) {
+	panic("implement me")
+}
+
+func (os *OrderService) Create(context.Context, *dto.OrderRequest) (*dto.OrderResponse, error) {
+	panic("implement me")
+}
+
+func (os *OrderService) HandlePayment(context.Context, *dto.PaymentData) error {
+	panic("implement me")
+}
