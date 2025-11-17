e3166387a6f7be10466b9e01a0beabaf50b2ba8d
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-11-17 10:18:41 +0700

    refactor: update Order model to use OrderStatus type

--- domain/models/order.go
+++ domain/models/order.go
@@ -8,16 +8,16 @@ import (
 )
 
 type Order struct {
-	ID        uint                        `gorm:"primaryKey;autoIncrement"`
-	UUID      uuid.UUID                   `gorm:"type:uuid;not null"`
-	Code      string                      `gorm:"type:varchar(30);not null"`
-	UserId    uuid.UUID                   `gorm:"type:uuid;not null"`
-	PaymentID uuid.UUID                   `gorm:"type:uuid;not null"`
-	Amount    float64                     `gorm:"type:decimal(10,2);not null"`
-	Status    constants.OrderStatusString `gorm:"type:int;not null"`
-	Date      time.Time                   `gorm:"type:timestamp;not null"`
-	IsPaid    bool                        `gorm:"type:boolean;not null"`
-	PaidAt    *time.Time                  `gorm:"type:timestamp"`
+	ID        uint                  `gorm:"primaryKey;autoIncrement"`
+	UUID      uuid.UUID             `gorm:"type:uuid;not null"`
+	Code      string                `gorm:"type:varchar(30);not null"`
+	UserId    uuid.UUID             `gorm:"type:uuid;not null"`
+	PaymentID uuid.UUID             `gorm:"type:uuid;not null"`
+	Amount    float64               `gorm:"type:decimal(10,2);not null"`
+	Status    constants.OrderStatus `gorm:"type:int;not null"`
+	Date      time.Time             `gorm:"type:timestamp;not null"`
+	IsPaid    bool                  `gorm:"type:boolean;not null"`
+	PaidAt    *time.Time            `gorm:"type:timestamp"`
 	CreatedAt *time.Time
 	UpdatedAt *time.Time
 }
