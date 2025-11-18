c15c79732149038fe0e30aceef578f953edd1717
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-11-18 12:56:29 +0700

    implement Registry service with NewRegistryService and GetOrder methods

--- services/registry.go
+++ services/registry.go
@@ -1,1 +1,27 @@
 package services
+
+import (
+	"order-service/clients"
+	"order-service/repositories"
+	services "order-service/services/order"
+)
+
+type Registry struct {
+	repository repositories.IRegistryRepository
+	client     clients.IRegistryClient
+}
+
+type IRegistryService interface {
+	GetOrder() services.IOrderService
+}
+
+func NewRegistryService(repository repositories.IRegistryRepository, client clients.IRegistryClient) IRegistryService {
+	return &Registry{
+		repository: repository,
+		client:     client,
+	}
+}
+
+func (r *Registry) GetOrder() services.IOrderService {
+	return services.NewOrderService(r.repository, r.client)
+}
