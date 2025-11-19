f73bce9a2f33101a6ca34bb693572ef417122359
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-11-19 14:36:00 +0700

    implement OrderController with pagination and validation

---
+++ controllers/http/order/registry.go
@@ -0,0 +1,76 @@
+package constrollers
+
+import (
+	"net/http"
+	errConst "order-service/common/error"
+	"order-service/common/response"
+	"order-service/domain/dto"
+	"order-service/services"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+type OrderController struct {
+	service services.IRegistryService
+}
+
+type IOrderController interface {
+	GetAllWithPagination(*gin.Context)
+	GetByUUID(*gin.Context)
+	GetOrderByUserID(*gin.Context)
+	Create(*gin.Context)
+}
+
+func NewOrderController(service services.IRegistryService) IOrderController {
+	return &OrderController{service: service}
+}
+
+func (oc *OrderController) GetAllWithPagination(ctx *gin.Context) {
+	var params dto.OrderRequestParam
+	err := ctx.ShouldBindJSON(&params)
+	if err != nil {
+		response.HttpResponse(response.ParamHTTPResponse{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+		return
+	}
+
+	validate := validator.New()
+	if err = validate.Struct(params); err != nil {
+		errMessage := http.StatusText(http.StatusUnprocessableEntity)
+		errResponse := errConst.ErrValidationResponse(err)
+		response.HttpResponse(response.ParamHTTPResponse{
+			Err:     err,
+			Code:    http.StatusUnprocessableEntity,
+			Message: &errMessage,
+			Data:    errResponse,
+			Gin:     ctx,
+		})
+		return
+	}
+
+	result, err := oc.service.GetOrder().GetAllWithPagination(ctx, &params)
+	if err != nil {
+		response.HttpResponse(response.ParamHTTPResponse{
+			Code: http.StatusBadRequest,
+			Err:  err,
+			Gin:  ctx,
+		})
+		return
+	}
+
+	response.HttpResponse(response.ParamHTTPResponse{
+		Code: http.StatusOK,
+		Data: result,
+		Gin:  ctx,
+	})
+}
+
+func (oc *OrderController) GetByUUID(*gin.Context) {}
+
+func (oc *OrderController) GetOrderByUserID(*gin.Context) {}
+
+func (oc *OrderController) Create(*gin.Context) {}
